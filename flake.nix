9e1a135a28e6820731d2b70cbdc32fe7953e4925
ILLEIST <rlopperman@proton.me>
2025-01-22 13:59:52 -0700

    Prepare repo for sharing

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Basic template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs  systems (system: f system);
+    in
+  {
+    devShells = forEachSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            pandoc
+            texliveFull
+            (python3.withPackages (ps: with ps; with python3Packages; [
+              jupyter
+              ipython
+              matplotlib
+              numpy
+              scipy
+            ]))
+          ];
+        };
+      });
+  };
+}
